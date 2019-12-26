535ea0489b85e23a833e21c7ae6338114eb40090
sujanodari <odarisujan@gmail.com>
2019-12-26 22:03:11 +0545

    Login user using api through server using phone number and password

--- app/src/main/java/com/sujan/break_o_last/LoginActivity.java
+++ app/src/main/java/com/sujan/break_o_last/LoginActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,7 +17,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sujan.break_o_last.api.HotelAPI;
+import com.sujan.break_o_last.models.CreateUser;
 import com.sujan.break_o_last.ui.registration.RegistrationActivity;
+import com.sujan.break_o_last.url.BaseUrl;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -23,11 +32,12 @@ public class LoginActivity extends AppCompatActivity {
     EditText Username, Password;
     Button Login;
     TextView tvSignup;
-    String user, pass;
+    String user="", pass="";
     LinearLayout myLayout;
     AnimationDrawable animationDrawable;
     CheckBox loginCheck;
-
+    String Name,Address,Email,Cpass,Gender;
+    Bitmap Profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,22 +74,33 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pass)) {
 
-                    if (user.equals("admin") && pass.equals("admin")) {
-                        if(loginCheck.isChecked()){
-                        setPreferences();}
-                        Username.setText("");
-                        Password.setText("");
+                    CreateUser rg =new CreateUser(Name,user,Address,Email,pass,Cpass,Gender,"Profile");
+                    HotelAPI hotelAPI= BaseUrl.getInstance().create(HotelAPI.class);
+                    retrofit2.Call<Void> voidCall=hotelAPI.login(rg);
+                    voidCall.enqueue(new Callback<Void>() {
+                        @Override
+                        public void onResponse(Call<Void> call, Response<Void> response) {
+                            if (!response.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "User not found" , Toast.LENGTH_SHORT).show();
+                                Log.d("error", "error" + response.code());
+                                return;
+                            }
+                            if(loginCheck.isChecked()){
+                                setPreferences();}
+                            Username.setText("");
+                            Password.setText("");
 
-                        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                        intent.putExtra(EXTRA_MESSAGE, user);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
-                        Username.setText("");
-                        Password.setText("");
-                    }
+                            Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                            intent.putExtra(EXTRA_MESSAGE, user);
+                            startActivity(intent);
+                        }
 
+                        @Override
+                        public void onFailure(Call<Void> call, Throwable t) {
+                            Toast.makeText(LoginActivity.this, "Error"+t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
 
+                        }
+                    });
                 } else {
                     if (TextUtils.isEmpty(user)) {
                         Username.setError("Enter Username");
